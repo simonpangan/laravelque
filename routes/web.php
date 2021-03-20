eaa6c145a88fe54e2d451da12a65541ebfd5a21a
Simon Pangan <simonjoseph.pangan@gmail.com>
2021-03-20 22:45:39 +0800

    Mail

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Mail\WelcomeMail;
+Route::get('/mail', function () {
+    Mail::to('EmailAddress@EmailAddress.com')->send(new WelcomeMail()); 
+});
