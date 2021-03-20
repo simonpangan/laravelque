c9d033fb3e8d750966f447d2da2ef0836a63bc71
Simon Pangan <simonjoseph.pangan@gmail.com>
2021-03-20 23:06:46 +0800

    Database queue

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class WelcomeMail extends Mailable
+class WelcomeMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -28,6 +28,7 @@ class WelcomeMail extends Mailable
      */
     public function build()
     {
+        sleep(10);
         return $this->markdown('emails.welcome');
     }
 }
